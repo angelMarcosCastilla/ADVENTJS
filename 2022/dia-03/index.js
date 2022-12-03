f941ca4d1eae097425ca6314ad08087a83f31c14
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-12-03 14:32:30 -0500

    solution day 3

---
+++ 2022/dia-03/index.js
@@ -0,0 +1,11 @@
+function distributeGifts(packOfGifts, reindeers) {
+  function distributeGifts(packOfGifts, reindeers) {
+    const reindeersLength = reindeers.reduce((acc, el) => {
+      return acc += (el.length * 2)
+    }, 0)
+    const giftsLength = packOfGifts.reduce((acc, el) => {
+      return acc += el.length
+    }, 0)
+    return Math.floor(reindeersLength / giftsLength);
+  }
+}
