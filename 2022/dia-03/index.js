082c12c0f4e3b04f2af88b640cf457d2c1140d39
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-12-03 14:33:20 -0500

    solution day 3

--- 2022/dia-03/index.js
+++ 2022/dia-03/index.js
@@ -1,11 +1,9 @@
 function distributeGifts(packOfGifts, reindeers) {
-  function distributeGifts(packOfGifts, reindeers) {
-    const reindeersLength = reindeers.reduce((acc, el) => {
-      return acc += (el.length * 2)
-    }, 0)
-    const giftsLength = packOfGifts.reduce((acc, el) => {
-      return acc += el.length
-    }, 0)
-    return Math.floor(reindeersLength / giftsLength);
-  }
+  const reindeersLength = reindeers.reduce((acc, el) => {
+    return acc += (el.length * 2)
+  }, 0)
+  const giftsLength = packOfGifts.reduce((acc, el) => {
+    return acc += el.length
+  }, 0)
+  return Math.floor(reindeersLength / giftsLength);
 }
