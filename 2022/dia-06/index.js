56f8e2c63a1d37c361cbe339bce4551dc5ac2987
Angel Marcos Castilla <78944493+angelMarcosCastilla@users.noreply.github.com>
2022-12-06 18:32:40 -0500

    solution day 06

--- 2022/dia-06/index.js
+++ 2022/dia-06/index.js
@@ -3,20 +3,19 @@ function createCube(size) {
   const rigthTop = "_\\".repeat(size);
   const leftBottom = "\\/";
   const rigthBottom = "_/".repeat(size);
-  let top ="";
-  let bottom = ""
-  
-  for(let i = 1; i <= size; i++) {
-    top+=`${" ".repeat(size-i)}${lefTop.repeat(i)}${rigthTop}\n`;
-    bottom+=`${" ".repeat(( i + size - 1) - size)}${leftBottom.repeat((size + 1)-i)}${rigthBottom}`
-    bottom+= i < size ?  "\n" : "";
-    
+  let top = [];
+  let bottom = []
+
+  for (let i = 1; i <= size; i++) {
+    top.push(`${" ".repeat(size - i)}${lefTop.repeat(i)}${rigthTop}`);
+    bottom.push(`${" ".repeat((i + size - 1) - size)}${leftBottom.repeat((size + 1) - i)}${rigthBottom}`)
+
   }
-  
-  return `${top}${bottom}`
+
+  return [...top, ...bottom].join(`\n`)
 }
 
-console.log(createCube(3))
+
 
 // output:
 /*
