fbb0f0eddfb94ea746b9d147127544331c103b58
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-12-08 13:27:30 -0500

    solution day 08

---
+++ 2022/dia-08/index.js
@@ -0,0 +1,21 @@
+function checkPart(part) {
+  function checkPart(part) {
+    return Array.from({ length: part.length }).some((_, index) => {
+      const path = part.slice(0, index) + part.slice(index + 1);
+      const pathReverse = path.split("").reverse().join("");
+      return path === pathReverse
+    });
+  }
+
+}
+
+checkPart("uwu") // true
+// "uwu" es un palíndromo sin eliminar ningún carácter
+
+checkPart("miidim") // true
+// "miidim" puede ser un palíndromo después de eliminar la primera "i"
+// ya que "midim" es un palíndromo
+
+checkPart("midu") // false
+
+checkPart("zzzoonzzz")
