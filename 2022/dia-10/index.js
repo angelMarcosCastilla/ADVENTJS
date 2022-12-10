40a1b2f4ddfbebcef2af13ac81640d5ef0f9d403
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-12-10 15:47:20 -0500

    solution day 10

--- 2022/dia-10/index.js
+++ 2022/dia-10/index.js
@@ -1,42 +1,21 @@
 function checkJump(heights) {
-  if (heights[0] >= heights[1]) {
-    return false;
-  }
-
-  const numMayor = heights.findIndex((num, index) => {
-    return num > heights[index + 1];
-  });
-
-  const arrAsc = heights.slice(0, numMayor);
-  const arrDesc = heights.slice(numMayor, -1);
-
-  const isAsendent = arrAsc.every((num, index) => {
+  const numMayor = heights.indexOf(Math.max(...heights));
+  const arrAsc = heights.slice(0, numMayor );
+  const arrDesc = heights.slice(numMayor + 1);
+  const isDesendent = arrDesc.length > 0 && arrDesc.every((num, index) => {
+    return num <= heights[numMayor + index];
+  })
+  const isAscendent = arrAsc.length > 0 && arrAsc.every((num, index) => {
     return num <= heights[index + 1];
   });
-
-  const isDesendent = arrDesc.length > 0 && arrDesc.every((num, index) => {
-    return num >= heights[numMayor + index + 1];
-  })
-
-
-  return [isAsendent, isDesendent].every(Boolean);
+  return isDesendent && isAscendent
 }
 
-
-// version 2.0 
-function checkJump(heights) {
-  if (heights[0] >= heights[1]) {
-    return false;
-  }
-
-  const numMayor = heights.findIndex((num, index) => {
-    return num > heights[index + 1];
-  });
-
-  const arrDesc = heights.slice(numMayor, -1);
-  const isDesendent = arrDesc.length > 0 && arrDesc.every((num, index) => {
-    return num >= heights[numMayor + index + 1];
-  })
-  return isDesendent;
-
-}
+console.log(checkJump([1, 2, 1])) //true
+console.log(checkJump([1, 3, 8, 5, 2])) // true
+console.log(checkJump([1, 7, 3, 5])) // false
+console.log(checkJump([2,2,2,2,2])) // false
+console.log(checkJump([1, 2, 2, 2, 1])) // }rtue
+console.log(checkJump([1, 1, 1, 1, 1, 1, 1, 1, 2, 1])) // true
+console.log(checkJump([ 1,3, 2, 5, 4, 3, 2, 1])) // false
+console.log(checkJump([ 1,2,3,9,5,6,8])) // false
