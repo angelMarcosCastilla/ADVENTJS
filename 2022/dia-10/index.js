3112c0c9b0a0b22c2ce72668b59fb87ce00ea8a5
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-12-10 13:25:18 -0500

    solution day 10

---
+++ 2022/dia-10/index.js
@@ -0,0 +1,23 @@
+function checkJump(heights) {
+  if (heights[0] >= heights[1]) {
+    return false;
+  }
+
+  const numMayor = heights.findIndex((num, index) => {
+    return num > heights[index + 1];
+  });
+
+  const arrAsc = heights.slice(0, numMayor);
+  const arrDesc = heights.slice(numMayor, -1);
+
+  const isAsendent = arrAsc.every((num, index) => {
+    return num <= heights[index + 1];
+  });
+
+  const isDesendent = arrDesc.length > 0 && arrDesc.every((num, index) => {
+    return num >= heights[numMayor + index + 1];
+  })
+
+
+  return [isAsendent, isDesendent].every(Boolean);
+}
