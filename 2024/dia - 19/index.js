3c6df3bc5b1e10fa07c0c632e24c826dff52af5e
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-12-19 18:57:58 -0500

    add day 19

---
+++ 2024/dia - 19/index.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number} weight - Total weight of the gifts
+ * @returns {string} - Stacked boxes represented as ASCII art
+ */
+function distributeWeight(weight) {
+  const boxRepresentations = {
+    1: [" _ ", "|_|"],
+    2: [" ___ ", "|___|"],
+    5: [" _____ ", "|     |", "|_____|"],
+    10: [" _________ ", "|         |", "|_________|"],
+  };
+  const boxes = [10, 5, 2, 1];
+  const result = {
+    10: 0,
+    5: 0,
+    2: 0,
+    1: 0,
+  };
+
+  let draw = [];
+
+  for (const box of boxes) {
+    if (weight >= box) {
+      let boxDraw = [];
+      result[box] = Math.floor(weight / box);
+      weight %= box;
+      let baseBox = boxRepresentations[box].at(-1);
+      const topBox = boxRepresentations[box][0];
+      let middleBox = box >= 5 ? boxRepresentations[box][1] : "";
+      boxDraw.push(topBox);
+      for (let i = 0; i < result[box]; i++) {
+        if (middleBox) {
+          boxDraw.push(middleBox);
+        }
+        boxDraw.push(baseBox);
+      }
+
+      draw.unshift(boxDraw);
+      const prevDraw = draw[1];
+      let current = draw[0];
+      if (prevDraw) {
+        draw[1] = prevDraw.slice(1);
+        const length = prevDraw[0].length - current[0].length - 1;
+        current[current.length - 1] += "_".repeat(length);
+      }
+    }
+  }
+
+  return draw.map((box) => box.join("\n")).join("\n");
+}
