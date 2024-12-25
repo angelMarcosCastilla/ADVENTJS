a1c4f52eaeb6a7e0f799454ded3ad54a89420b6d
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-12-25 12:15:58 -0500

    add day 26

---
+++ 2024/dia - 26/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} timeWorked - Time worked in hh:mm:ss format.
+ * @param {string} totalTime - Total estimated time in hh:mm:ss format.
+ * @returns {string} - The completed percentage rounded to the nearest integer with a % sign.
+ */
+function getCompleted(timeWorked, totalTime) {
+  const toSeconds = (time) => {
+    const [hours, minutes, seconds] = time.split(':').map(Number);
+    return hours * 3600 + minutes * 60 + seconds;
+  };
+
+  const workedSeconds = toSeconds(timeWorked);
+  const totalSeconds = toSeconds(totalTime);
+
+  const percentage = (workedSeconds / totalSeconds) * 100;
+
+  return Math.round(percentage)+"%";
+}
